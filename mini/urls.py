e72cc88cebc2f20795157d9d0df0e0c09a6bfddc
ShaikRiyaz786r <reehuriyaz786@gmail.com>
2023-12-01 10:57:05 +0530

    final commit

--- mini/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from myapp.views import firstView,secondView
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('login/', firstView),
-    path('signup/',secondView),
-]
